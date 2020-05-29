6634f06e1fcf701e8c23afe46858728490482695
scymax <66103776+scymax@users.noreply.github.com>
2020-05-29 19:33:20 +0100

    Add files via upload

--- EnchantedForestUpgradesAndDrones.py
+++ EnchantedForestUpgradesAndDrones.py
@@ -4,7 +4,7 @@ import time
 import keyboard
 import random
 import win32api, win32con
-
+#74, 108, 227
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
@@ -22,20 +22,9 @@ while keyboard.is_pressed('q') == False:
             r,g,b = pic.getpixel((x,y))
 
             if (
-                   (r in range(200,230) and (g in range(70,90) and                                            
-                   (b in range(90,110))))):
+                   r == 221  and g == 85 and                                            
+                   b == 102):
                 pg.click(x+750,y+290)
+                time.sleep(0.1)
                 pg.click(1165, 360)
-                time.sleep(0.05)
-                pg.click(880, 370)
-                pg.click(1150, 940)
-                pg.click(1200, 720)
-                pg.click(880, 440)
-                pg.click(1150, 940)
-                pg.click(1200, 720)
-                pg.click(830, 950)
-                pg.click(920, 950)
-                pg.click(1010, 950)
-                pg.click(1100, 950)
-                time.sleep(2)
                 break
